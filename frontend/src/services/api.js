3cf40e371b8ea44f4f9a9aa6e686b2b3cf15eba3
Nisrine El Harkani <harkaninissrine81@gmail.com>
2025-09-14 15:01:38 +0100

    add api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// Configuring Axios instance for backend API
-// Base URL points to Django backend
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://trip-management.railway.app/api/",
 });
 
 export default api;
